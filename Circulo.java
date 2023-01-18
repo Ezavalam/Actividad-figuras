c8b81394ed096063db549180b5dbc66c8bdd3b10
Emilio <122295142+Ezavalam@users.noreply.github.com>
2023-01-18 16:11:42 -0600

    Add files via upload

---
+++ Circulo.java
@@ -0,0 +1,23 @@
+package cuadrado;
+
+public class Circulo {
+
+	public int radio;
+	public float circunferencia;
+	public float area;
+	public float id;
+
+	public Circulo(int radio) {
+		this.radio = radio;
+	}
+	public float calcularCircunferencia() {
+		float pi = (float) Math.PI;
+		float circun = 2*pi*radio;
+		return circun;
+	}
+	public float calcularArea() {
+		float pi = (float) Math.PI; 
+		float area = pi*radio*radio ;
+		return area;
+	}
+}
---
+++ Cuadrado.java
@@ -0,0 +1,25 @@
+package cuadrado;
+
+public class Cuadrado {
+	
+	public int longuitud;
+	public float diagonal;
+	public float perimetro;
+	public float id;
+	
+	public Cuadrado(int longuitud) {
+		this.longuitud = longuitud;	}
+	
+	public double calculaDiagonal() {
+		return Math.sqrt(longuitud*longuitud + longuitud*longuitud);
+	}
+	
+	public float calculaPerimetro() {
+		return longuitud*4;
+	}
+	
+	public float calculaArea() {
+		return longuitud*longuitud;
+	}
+	
+}
