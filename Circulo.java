64a0ce5b8c03a917b42dc7314f7326617f4edc69
Emilio <122295142+Ezavalam@users.noreply.github.com>
2023-01-25 11:01:40 -0600

    Update Circulo.java

--- Circulo.java
+++ Circulo.java
@@ -1,7 +1,7 @@
 package actividad4;
 
 public class Circulo {
-	public float radio;
+	public double radio;
 	public float circunferencia;
 	public float area;
 	public float id;
@@ -10,19 +10,19 @@ public class Circulo {
 		this(176895);
 	}
 
-	public Circulo(float radio) {
+	public Circulo(double radio) {
 		this.radio = radio;
 	}
 
 	
-	public float calcularCircunferencia() {
-		float pi = (float) Math.PI;
-		float circun = 2*pi*radio;
+	public double calcularCircunferencia() {
+		double pi = (float) Math.PI;
+		double circun = 2*pi*radio;
 		return circun;
 	}
-	public float calcularArea() {
-		float pi = (float) Math.PI; 
-		float area = pi*radio*radio ;
+	public double calcularArea() {
+		double pi = (float) Math.PI; 
+		double area = pi*radio*radio ;
 		return area;
 	}
 }
